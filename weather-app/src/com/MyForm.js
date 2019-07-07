339e2985e80535e9728ebae922cd69b681439ed3
shadabali604 <shadabali604@gmail.com>
2019-07-07 14:34:08 +0530

    Beta Version

--- weather-app/src/com/MyForm.js
+++ weather-app/src/com/MyForm.js
@@ -4,11 +4,11 @@ function MyFrom (props){
     return(
         <div className = "Form-section">
         <form onSubmit = {props.weather}>
+        <label for = "city">City:</label>
         <input type = "text" name="city" placeholder = "Enter City "></input>
 
-        
+        <label for = "country">Country:</label>
         <input type = "text" name="country" placeholder = 'Enter Country'></input>
-
 
         <input type = 'submit' value = 'submit' className = "btn-submit"></input>
         </form>
