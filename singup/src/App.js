8dbefcdb98cb97d270a7e82d29f83f5eaf9325a9
shadabali604 <shadabali604@gmail.com>
2019-09-15 23:35:29 +0530

    Change

--- singup/src/App.js
+++ singup/src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
 render(){
+ 
   return(
-    <div className = 'Form'>
+    <div className = 'Form' onSubmit = {this.handleSubmit}>
     <form>
-    <input type ="text" placeholder =" Enter Your Yame"></input><br></br>
+    <input type ="text" placeholder =" Enter Your Yame" value  = {this.state.name}></input><br></br>
     <input type = "email" placeholder = "Enter Your Email"></input><br></br>
     <button type = 'submit' value = 'Submit Now'>Submit</button>
     </form>
     </div>
   )
 }
-
+handleSubmit(){
+     
+}
 }
  
+nowHandle 
 
 export default App;
