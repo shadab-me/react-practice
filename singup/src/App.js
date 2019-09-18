a2676b0e2c01b6792f1b0eaa5a82b1ac484c1c2f
shadabali604 <shadabali604@gmail.com>
2019-09-18 09:48:27 +0530

    Form

--- singup/src/App.js
+++ singup/src/App.js
@@ -1,27 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 class App extends React.Component{
   constructor(props){
     super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+
+
   }
+
+  handleChange(e){
+      let target = e.target;
+      let value = target.type === 'email'? '': target.value; 
+      let password = target.password
+      
+      this.setState({
+        [email]: value,
+        [password]: password
+      })
+  }
+  
+
+
 render(){
  
   return(
-    <div className = 'Form' onSubmit = {this.handleSubmit}>
-    <form>
-    <input type ="text" placeholder =" Enter Your Yame" value  = {this.state.name}></input><br></br>
-    <input type = "email" placeholder = "Enter Your Email"></input><br></br>
-    <button type = 'submit' value = 'Submit Now'>Submit</button>
-    </form>
+    <div className = "App">
+     <div className = "App__Aside"></div>
+     <div className = "App__Form">
+     
+     <form onSubmit = {this.handleSubmit}>
+
+     <input type = 'Email' placeholder = 'Enter Your Email' value = {this.state.email} onChange = {this.handleChange}></input>
+     <br></br>
+     <input type = 'password' placeholder = 'Enter Your password' value = {this.state.password} onChange  = {this.handleChange}></input>
+     <br></br>
+     <button type = 'submit'>Submit</button>
+     </form>
+     
+     </div>
+
+
     </div>
   )
-}
-handleSubmit(){
-     
-}
-}
- 
-nowHandle 
 
+  }
+}
 export default App;
