7ba7a306c90cc51de9007762cbec257eaf832ee3
shadabali604 <shadabali604@gmail.com>
2019-10-06 01:15:06 +0530

    update

--- singup/src/App.js
+++ singup/src/App.js
@@ -3,53 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
 
-    this.state = {
-      email: '',
-      password: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
+ constructor(props) {
+   super(props);
+   this.state = {count: 0};
+ }
 
+ tick(){
+  this.setState({
+    count: count + 1,
+  });
 
-  }
+}
 
-  handleChange(e){
-      let target = e.target;
-      let value = target.type === 'email'? '': target.value; 
-      let password = target.password
-      
-      this.setState({
-        [email]: value,
-        [password]: password
-      })
-  }
-  
+render() {
+return(
+
+    <div>
+    <h1>{this.state.count}</h1>
+<button onClick = {this.tick}>Click Me</button>
+</div>
 
 
-render(){
- 
-  return(
-    <div className = "App">
-     <div className = "App__Aside"></div>
-     <div className = "App__Form">
-     
-     <form onSubmit = {this.handleSubmit}>
-
-     <input type = 'Email' placeholder = 'Enter Your Email' value = {this.state.email} onChange = {this.handleChange}></input>
-     <br></br>
-     <input type = 'password' placeholder = 'Enter Your password' value = {this.state.password} onChange  = {this.handleChange}></input>
-     <br></br>
-     <button type = 'submit'>Submit</button>
-     </form>
-     
-     </div>
-
-
-    </div>
   )
+}
 
-  }
+
 }
 export default App;
