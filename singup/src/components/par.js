8398adb0fac5fcd73908d4a248d673f95f8f780c
shadabali604 <shadabali604@gmail.com>
2019-10-06 23:17:57 +0530

    learn props

---
+++ singup/src/components/par.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import RectDOM from 'react-dom';
+
+class Par extends React.Component {
+render(){
+    return(
+        <div>
+        
+        <h1>{this.props.count}</h1>
+        
+        </div>   
+         )
+}
+}
+export default Par;
