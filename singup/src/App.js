7603e72decf6ba4361cd284cfe8a77a6e19cd490
shadabali604 <shadabali604@gmail.com>
2019-10-09 23:11:26 +0530

    update

--- singup/src/App.js
+++ singup/src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+ import Par from './components/par';
+import Todo from './components/todo';
+import Nav from './components/nav';
 class App extends React.Component{
-
- constructor(props) {
+  constructor(props) {
    super(props);
-   this.state = {count: 0};
+   this.state = {
+     count: 0
+  };
+
+ this.tick = this.tick.bind(this);
+
  }
 
  tick(){
   this.setState({
-    count: count + 1,
+  count: this.state.count+1,
   });
 
 }
 
 render() {
 return(
-
-    <div>
-    <h1>{this.state.count}</h1>
+  <div className = ''>
+<Nav/>
+<Todo/>
+<Par count  = {this.state.count}/>
+<h1>{this.state.count}</h1>
 <button onClick = {this.tick}>Click Me</button>
+
 </div>
-
-
-  )
-}
-
-
-}
+)
+}}
 export default App;
---
+++ singup/src/components/todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../components/todo.css';
+
+class Todo extends React.Component{
+    
+    constructor(){
+        super();
+ 
+    }
+        render(){
+        return(
+            <div className = 'ToDo'>
+            <h1>To Do list</h1>
+            <input type = 'text'></input>    
+            <button>+</button>
+   <ul></ul>
+            </div>
+        )}
+}
+export default Todo;
